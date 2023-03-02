3642dee2ad811670d00575621c15d1104bb400af
Andrea Fermo <andreafermo.sd@gmail.com>
2023-03-02 16:39:19 +0100

    esercizio palindromo concluso

---
+++ palindroma/js/main.js
@@ -0,0 +1,30 @@
+const parola = prompt('inserisci una parola');
+console.log(parola);
+
+let palindromia = invertiParola()
+console.log(palindromia)
+
+
+function invertiParola() {
+
+let parolaInvertita = '';
+let risultato = '';
+
+for (i = parola.length - 1; i >= 0  ; i--) {
+    
+    parolaInvertita += parola[i];
+    console.log(parolaInvertita);
+  
+}
+
+if (parolaInvertita == parola) {
+    risultato = 'la parola è palindroma';
+} else {
+    risultato = 'la parola non è palindroma';
+}
+
+return risultato;
+
+
+    
+}
